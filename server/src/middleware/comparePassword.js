0b265cfffb0c6a270bea0f93c3ed9bf3f601b7c1
electricalveins <88440918DjdfCfif>
2020-03-29 13:00:05 +0300

    add middlewares
    findChatById

--- server/src/middleware/comparePassword.js
+++ server/src/middleware/comparePassword.js
@@ -1,7 +1,6 @@
 const { BadRequestError } = require('../utils/errors');
 module.exports = async (req, res, next) => {
   try {
-
     if (await req.user.comparePassword(req.body.password)) {
 
       return next();
---
+++ server/src/middleware/findChatById.js
@@ -0,0 +1,9 @@
+const Chat = require('./../db/models/Chat.js');
+
+module.exports.findChatById = async (req, res, next) => {
+    req.chat = await Chat.findById(req.params.chatId);
+    if (req.chat) {
+        return next();
+    }
+    return res.status(404).send('Chat Not Found');
+};
