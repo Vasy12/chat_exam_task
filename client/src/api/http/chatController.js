ffda541febd1e19d4f329b27eacec7b3a80e6380
eveins <maxmost512@gmail.com>
2020-03-29 21:57:29 +0300

    add chat http api

--- client/src/api/http/chatController.js
+++ client/src/api/http/chatController.js
@@ -15,3 +15,23 @@ export const getChatMessages = ( { chatId } ) =>
       'Content-type': 'application/json',
     }
   } );
+
+export const getAllAvailableChats = () => http.get( '/chats', {
+  headers: {
+    'Content-type': 'application/json',
+  }
+} );
+
+export const joinUserToChatById = ( chatId, userId ) =>
+  http.post( `/chat/${chatId}/participants`, null, {
+    headers: {
+      'Authorization': userId,
+    },
+  } );
+
+export const leaveChatById = ( chatId, userId ) => http.delete( `/chat/${chatId}/participants`,
+  {
+    headers: {
+      'Authorization': userId,
+    },
+  } );
