846e1bfb365b6d1b37ba725f388231485f555b6d
eveins <maxmost512@gmail.com>
2020-03-30 17:57:07 +0300

    add create validation

--- client/src/components/forms/CreateChatForm/index.js
+++ client/src/components/forms/CreateChatForm/index.js
@@ -12,7 +12,7 @@ const MessageForm = ( props ) => {
   const { userId } = props;
 
   const messageSchema = Yup.object().shape( {
-    name: Yup.string().min( 4, 'Empty name' ).required( 'EnterMessage' )
+    name: Yup.string().min( 4, 'Chat name must contain at least 4 symbols' )
   } );
 
   const handleSubmit = ( { name }, formikBag ) => {
@@ -27,12 +27,17 @@ const MessageForm = ( props ) => {
               name: ''
             }}>
       {
-        ( {} ) => (
+        ( { errors, touched } ) => (
           <Form>
             <Field name={'name'}
                    type={'text'}
                    placeholder={'Type Chat Name Here:'}/>
-
+            {
+              errors.name && touched.name
+              ? ( <div>{errors.name}</div> )
+              : null
+            }
+            <br/>
             <button type={'submit'}>Send</button>
           </Form>
         )
